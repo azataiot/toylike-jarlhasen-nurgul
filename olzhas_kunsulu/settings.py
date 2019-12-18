4977a431e0343ca15743b99e80395d26996bf5e7
Yaakov Azat <yaakovazat@gmail.com>
2019-12-18 20:38:30 +0600

    update ig

--- olzhas_kunsulu/settings.py
+++ olzhas_kunsulu/settings.py
@@ -114,7 +114,13 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static')
